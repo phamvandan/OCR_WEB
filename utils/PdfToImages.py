278b770576bbcde8ef66615cb6984e58f6a0115d
trandat <trandat@pop-os.localdomain>
2019-11-28 19:26:33 +0700

    update

--- utils/PdfToImages.py
+++ utils/PdfToImages.py
@@ -5,7 +5,7 @@ import os
 def pdfToImage(pdf_file,output_folder):
         print("start")
         i=1
-        pdf = PdfFileReader(open(pdf_file,'rb'))
+        pdf = PdfFileReader(open(str(pdf_file),'rb'))
         maxPages = pdf.getNumPages()
         for page in range(1,maxPages+1,10) : 
                 images_from_path = convert_from_path(pdf_file, dpi=200, first_page=page, last_page = min(page+10-1,maxPages))
