a5c4f27fbabb84a84ae90bcc18a0966cca10d051
trandat <trandat@pop-os.localdomain>
2019-11-29 10:32:09 +0700

    update

--- server.py
+++ server.py
@@ -90,4 +90,4 @@ def upload_file():
 			return redirect('uploadFile')
 if __name__ == "__main__":
     # app.run(host='172.16.1.27',port=80)
-	app.run(host='10.42.49.111',port=80)
+	app.run(host='0.0.0.0',port=80)
