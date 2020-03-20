cabfb6d051703a97a4e59915bd8a366833e1b151
trandat <tranmanhdathsgs@gmail.com>
2020-03-20 11:32:45 +0700

    update test.py

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
+import os
+
 from utils.detai import ocr_file
 
-text_ocr = ocr_file("/home/trandat/project/eDocument/OCR_WEB/static/demo/84.pdf", True, True, True, False)
-
+text_ocr = ocr_file("static/demo/65.pdf", False, True, True, False)
+if os.path.isfile('text.txt'):
+	os.remove('text.txt')
+with open('text.txt', 'a+') as file:
+	file.write(text_ocr)
 # print(text_ocr)
